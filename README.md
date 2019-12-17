d497aa466cc7ef3526d49ce81d548903180044e6
Terciero <willard.hucks@gmail.com>
2019-12-16 17:03:59 -0800

    implement app factory pattern

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ https://flask-sqlalchemy.palletsprojects.com/en/2.x/
 https://docs.pytest.org/en/latest/getting-started.html
 https://pybit.es/pytest-fixtures.html
 https://docs.pytest.org/en/latest/fixture.html#fixture-finalization-executing-teardown-code
+https://flask.palletsprojects.com/en/1.1.x/patterns/appfactories/
--- project/__init__.py
+++ project/__init__.py
@@ -1,41 +1,30 @@
 import os
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
-# instantiate app
-app = Flask(__name__)
-
-api = Api(app)
-
-# set configuration
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # instantiate db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# models
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+# app factory
+def create_app(script_info=None):
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # instantiate app
+    app = Flask(__name__)
 
-# route handlers       
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'successs',
-            'message': 'pong!'
-        }
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    # set up extensions
+    db.init_app(app)
 
-api.add_resource(Ping, '/ping')
+    # register blueprints
+    from project.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
