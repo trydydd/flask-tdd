22f7c9fdb38382e497e45a5fe05ccff9d5e9886e
Terciero <willard.hucks@gmail.com>
2019-12-17 17:04:47 -0800

    Implement user endpoint tests and logic

--- project/tests/functional/test_users.py
+++ project/tests/functional/test_users.py
@@ -14,3 +14,50 @@ def test_add_user(test_app, test_database):
     assert resp.status_code == 201
     assert 'nope@nope.com was added!' in data['message']
     assert 'success' in data['status']
+
+def test_add_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data = json.dumps({}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Invalid payload.' in data['message']
+    assert 'fail' in data['status']
+
+def test_add_user_invalid_json_keys(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data = json.dumps({"test": "nope@nope.com"}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Invalid payload.' in data['message']
+    assert 'fail' in data['status']
+
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+    client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'trey',
+            'email': 'nope@nope.com'
+        }),
+        content_type='application/json',
+    )
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'trey',
+            'email': 'nope@nope.com'
+        }),
+        content_type='application/json'
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Sorry. That email already exists.' in data['message']
+    assert 'fail' in data['status']
